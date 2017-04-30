b47f4dc862baa21062f7f89103949d2347109876
YaddyVirus <yadullahabidi@gmail.com>
2017-04-30 13:25:38 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using System.Threading;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Solaris
+{
+    public sealed partial class MainPage : Page
+    {
+        private StreamSocket _socket;
+
+        private RfcommDeviceService _service;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Connected!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+            this.Frame.Navigate(typeof(page2));
+        }
+
+        
+
+
+    }
+}
---
+++ Solaris2.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>  
+SoftwareSerial BTSerial(10, 11);
+void setup() {
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  float voltage = sensorValue*(5.0/1024.0);
+  BTSerial.print("Voltage ");
+  BTSerial.println(voltage);
+  BTSerial.println("Current: 329 mA");
+  BTSerial.print("Output power: ");
+  BTSerial.print(voltage*329);
+  BTSerial.println("mW");
+  delay(5000);
+  BTSerial.println();
+
+}
